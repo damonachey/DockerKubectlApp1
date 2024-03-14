3de15c8b364bdc94d931df890fd790adf93796c4
Damon Achey <damon@achey.net>
2024-03-13 22:17:27 -0600

    Convert to mtn time

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         var app = WebApplication.Create(args);
 
-        app.MapGet("/", () => "Hello from App1.");
+        var nowMst = TimeZoneInfo.ConvertTime(DateTime.Now, TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time"));
+
+        app.MapGet("/", () => $"{nowMst:MM/dd/yyyy hh:mm:ss tt}: Hello from App1.");
 
         app.Run("http://+:5000");
     }
