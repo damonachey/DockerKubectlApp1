7c5046f34af3fbbc329c7ad873babcf3c79085bc
Damon Achey <damon@achey.net>
2024-03-14 09:53:54 -0600

    Fix kubeclt build run clean script
    - Wait form container state
    - Port forward
    - Delete pod

--- kubectl-build-run-cleanup.cmd
+++ kubectl-build-run-cleanup.cmd
@@ -1,11 +1,21 @@
 REM Build the container
 docker build --tag app1:latest .
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 REM Open the browser
 start "" "http://localhost:5000"
 
 REM Run the container
 REM * The call; command is used so CTRL+C can be used to stop the container but the script will continue
-kubectl run -i --rm app1container --image=app1:latest --port=5000 --image-pull-policy=Never & call;
+kubectl run app1container --image=app1:latest --port=5000 --expose=true --image-pull-policy=Never
+if %errorlevel% neq 1 exit /b %errorlevel%
 
-kubectl delete deployment app1container
+REM Wait for the container to be ready
+kubectl wait --for=condition=ready pod/app1container --timeout=30s
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+REM Port forward
+kubectl port-forward app1container 5000:5000 & call;
+
+REM Cleanup
+kubectl delete pods app1container
